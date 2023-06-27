608ec44937f9e9167f4f0282777b91a1652cc21e
Ragul Shanmugam <rshanmugam@enquizit.com>
2023-06-27 09:25:33 -0700

    Removing the console input logic to use inbuilt functions

--- cli/src/main/java/gov/cdc/dataingestion/commands/RegisterUser.java
+++ cli/src/main/java/gov/cdc/dataingestion/commands/RegisterUser.java
@@ -8,30 +8,48 @@ import java.io.Console;
 @CommandLine.Command(name = "register", mixinStandardHelpOptions = true, description = "Client will be onboarded providing username and secret.")
 public class RegisterUser implements Runnable {
 
-    @CommandLine.Option(names = {"--username"}, required = true, description = "Username provided by the client.")
+    @CommandLine.Option(names = {"--clientUsername"}, description = "Username provided by the client", interactive = true, echo = true, required = true)
     String username;
 
-    @CommandLine.Option(names = {"--secret"}, description = "Secret provided by the client", interactive = true)
+    @CommandLine.Option(names = {"--clientSecret"}, description = "Secret provided by the client", interactive = true, required = true)
     char[] password;
+
+    @CommandLine.Option(names = {"--adminUser"}, description = "Admin Username to connect to DI service", interactive = true, echo = true, required = true)
+    String adminUser;
+
+    @CommandLine.Option(names = {"--adminPassword"}, description = "Admin Password to connect to DI service", interactive = true, required = true)
+    char[] adminPassword;
 
 
     @Override
     public void run() {
-        Console console = System.console();
-        if (password == null && console != null) {
-            password = console.readPassword("Enter the secret provided by the client (CASE-SENSITIVE):");
+//        Console console = System.console();
+//        if (password == null && console != null) {
+//            password = console.readPassword("Enter the secret provided by the client (CASE-SENSITIVE):");
+//        }
+        if(username != null && password != null && adminUser != null && adminPassword != null) {
+            if(!username.isEmpty() && password.length > 0 && !adminUser.isEmpty() && adminPassword.length > 0) {
+                String serviceEndpoint = "https://dataingestion.datateam-cdc-nbs.eqsandbox.com/registration?username="
+                        + username + "&password=" + new String(password);
+                System.out.println("Connecting to NBS Data Ingestion Service...");
+//        String adminUser = console.readLine("Enter admin username: ");
+//        char[] adminPassword = console.readPassword("Enter admin password: ");
+                String apiResponse = AuthUtil.getString(adminUser, adminPassword, serviceEndpoint);
+//                System.out.println("Api response is..." + apiResponse);
+                if(apiResponse.contains("CREATED")) {
+                    System.out.println("User onboarded successfully.");
+                }
+                else if(apiResponse.contains("NOT_ACCEPTABLE")) {
+                    System.out.println("Username already exists. Please choose a unique client username.");
+                }
+            }
+            else {
+                System.err.println("One or more inputs are empty.");
+            }
         }
-        String serviceEndpoint = "https://dataingestion.datateam-cdc-nbs.eqsandbox.com/registration?username="
-                + username + "&password=" + new String(password);
-        System.out.println("Connecting to NBS Data Ingestion Service...");
-        String adminUser = console.readLine("Enter admin username: ");
-        char[] adminPassword = console.readPassword("Enter admin password: ");
-        String apiResponse = AuthUtil.getString(adminUser, adminPassword, serviceEndpoint);
-        if(apiResponse.contains("CREATED")) {
-            System.out.println("User onboarded successfully.");
+        else {
+            System.err.println("One or more inputs are null.");
         }
-        else if(apiResponse.contains("NOT_ACCEPTABLE")) {
-            System.out.println("Username already exists. Please choose a unique client username.");
-        }
+
     }
 }
--- cli/src/main/java/gov/cdc/dataingestion/commands/TokenGenerator.java
+++ cli/src/main/java/gov/cdc/dataingestion/commands/TokenGenerator.java
@@ -8,14 +8,31 @@ import java.io.Console;
 @CommandLine.Command(name = "token", mixinStandardHelpOptions = true, description = "Generates a JWT token.")
 public class TokenGenerator implements Runnable {
 
+    @CommandLine.Option(names = {"--adminUser"}, description = "Admin Username to connect to DI service", interactive = true, echo = true, required = true)
+    String adminUser;
+
+    @CommandLine.Option(names = {"--adminPassword"}, description = "Admin Password to connect to DI service", interactive = true, required = true)
+    char[] adminPassword;
+
     @Override
     public void run() {
-        Console console = System.console();
-        String serviceEndpoint = "https://dataingestion.datateam-cdc-nbs.eqsandbox.com/token";
-        System.out.println("Connecting to NBS Data Ingestion Service...");
-        String adminUser = console.readLine("Enter admin username: ");
-        char[] adminPassword = console.readPassword("Enter admin password: ");
-        String apiResponse = AuthUtil.getString(adminUser, adminPassword, serviceEndpoint);
-        System.out.println(apiResponse);
+//        Console console = System.console();
+        if(adminUser != null && adminPassword != null) {
+            if(!adminUser.isEmpty() && adminPassword.length > 0) {
+                String serviceEndpoint = "https://dataingestion.datateam-cdc-nbs.eqsandbox.com/token";
+                System.out.println("Connecting to NBS Data Ingestion Service...");
+//        String adminUser = console.readLine("Enter admin username: ");
+//        char[] adminPassword = console.readPassword("Enter admin password: ");
+                String apiResponse = AuthUtil.getString(adminUser, adminPassword, serviceEndpoint);
+                System.out.println(apiResponse);
+            }
+            else {
+                System.err.println("Admin username or password is empty.");
+            }
+        }
+        else {
+            System.err.println("Admin username or password is null.");
+        }
+
     }
 }
